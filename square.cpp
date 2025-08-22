80a82e94c8caba3dca232bea1bad02b058f51b4e
Arseniy <arseniy@neburalis.space>
2025-08-22 09:03:35 +0300

    Added some jokes

--- square.cpp
+++ square.cpp
@@ -11,7 +11,7 @@ double safe_get_double(char var){
         result = scanf("%lf", &a);
         while (getchar() != '\n');
         if (result) return a;
-        printf("Вы ввели число, которое я не смог распознать. Попробуйте ввести правильное число (например 12.34). Десятичный разделитель точка (.).\n");
+        printf("😢 Вы ввели число, которое я не смог распознать. Попробуйте ввести правильное число (например 12.34). Десятичный разделитель точка (.).\n");
     }
 }
 
@@ -19,38 +19,38 @@ int main(){
     // ax^2 + bx + c = 0
     double a, b, c;
 
-    printf("Программа для решения квадратного уравнения ax^2 + bx + c = 0\n");
-    printf("Введите коэффициенты a, b и c каждый в новой строке.\n");
+    printf("🧮 Программа для решения квадратного уравнения ax^2 + bx + c = 0\n");
+    printf("📝 Введите коэффициенты a, b и c каждый в новой строке.\n");
     a = safe_get_double('a');
     b = safe_get_double('b');
     c = safe_get_double('c');
 
-    printf("%lfx^2 + %lfx + %lfc = 0\n", a, b, c);
+    printf("📊 %lfx^2 + %lfx + %lfc = 0\n", a, b, c);
 
     if (fabs(a) > min_value){
         double d = b * b - 4 * a * c;
 
-        printf("d = %lf\n", d);
+        printf("🔍 d = %lf\n", d);
 
         if (d > 0){
             double x1 = (-b + sqrt(d))/(2*a);
             double x2 = (-b - sqrt(d))/(2*a);
-            printf("two solutions:\n"
+            printf("✅ two solutions:\n"
                 "x1 = %lf\n"
                 "x2 = %lf\n", x1, x2);
         }
         else if (fabs(d) < min_value){
             double x = (-b)/(2*a);
-            printf("one solution:\n"
+            printf("🎯 one solution:\n"
                 "x = %lf", x);
         }
         else if (d < 0){
-            printf("no solutions");
+            printf("❌ no solutions");
         }
     }
     else {
         double x = -(c / b);
-        printf("one solution:\n"
+        printf("🎯 one solution:\n"
             "x = %lf", x);
     }
 }
