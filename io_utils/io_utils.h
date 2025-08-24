71895b19bce5d20323c99b80a6ee8eb90994a9dc
Arseniy <arseniy@neburalis.space>
2025-08-24 14:38:47 +0300

    Add docstrings

--- io_utils/io_utils.h
+++ io_utils/io_utils.h
@@ -1,7 +1,42 @@
 #ifndef IO_UTILS_H
 #define IO_UTILS_H
 
+/**
+  @brief Функция отчистки буфера ввода
+  Считывает данные из потока ввода до \\n (\n также будет считан)
+
+  \code{cpp}
+    int a = 0;
+    a = getchar();
+
+    clear_stdin_buffer();
+    // Теперь буфер чист
+  \endcode
+
+  @return int 1 если буфер отчищен
+ */
 int clear_stdin_buffer();
+
+/**
+  @brief Функция проверки чист ли буфер ввода (очищает буфер если он чист)
+  Считывает данные из потока ввода до \\n (\n также будет считан)
+  Если считывается ' ' или '\t' - то считается что буфер чист
+  Если считывается любое другое, считается что буфер не чист
+
+
+  \code{cpp}
+    int a = 0, status = 0;
+    a = getchar();
+
+    if (is_stdin_buffer_clean())
+        // В буфере были только ' ' и '\t' теперь чист
+    else
+        // В буфере есть что-то еще, он остался не чист
+  \endcode
+
+  @return int 1 если буфер содержал только ' ' или '\t' или уже был чист
+  @return int 0 если буфер содержит что-то помимо пустоты
+ */
 int is_stdin_buffer_clean();
 double safe_get_double(const char * const var_name);
 int is_user_want_continue(const char * const ask_message);
