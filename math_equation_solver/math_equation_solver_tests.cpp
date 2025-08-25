389e6325db82e6979de1d531c2b27717dc0575f3
Arseniy <arseniy@neburalis.space>
2025-08-25 17:17:48 +0300

    Get tests from file

--- math_equation_solver/math_equation_solver_tests.cpp
+++ math_equation_solver/math_equation_solver_tests.cpp
@@ -65,59 +65,84 @@ int TEST_solve_square_equation(int * const count_tests) {
     (1, 2, 1) -> ONE, -1
     (1, 0, 2) -> NO
     */
+    FILE *fp = fopen("math_equation_solver/solve_square_equation_test_data.txt", "r");
+    struct TEST_DATA_solve_square_equation tests[100];
+    size_t test_count = 0;
+
+    uint16_t num = 0;
+    if (fp) {
+        for(int i = 0;;++i) {
+            double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+            int n_roots = 0;
+            if (fscanf(fp, "#%hd (%lf, %lf, %lf) -> %d, %lf, %lf", &num, &a, &b, &c, &n_roots, &x1, &x2)){
+                // printf("#%hd (%lf, %lf, %lf) -> %d, %lf, %lf\n", num, a, b, c, n_roots, x1, x2);
+
+                tests[i] = {
+                    .ss_test_n = num,
+                    .ss_kf_a = a, .ss_kf_b = b, .ss_kf_c = c,
+                    .ss_reference_nRoots = (solutions_count) n_roots,
+                    .ss_reference_ans1 = x1, .ss_reference_ans2 = x2,
+                };
+                ++test_count;
+            }
+
+            if (getc(fp) == EOF)
+                break;
+        }
+        fclose(fp);
+    }
 
     int is_failed = 0;
 
-    struct TEST_DATA_solve_square_equation tests[] = {
-        {
-            .ss_test_n           = 1,
-            .ss_kf_a             = 1,
-            .ss_kf_b             = -3,
-            .ss_kf_c             = 2,
-            .ss_reference_nRoots = TWO,
-            .ss_reference_ans1   = 1,
-            .ss_reference_ans2   = 2,
-        },
-        {
-            .ss_test_n           = 2,
-            .ss_kf_a             = 1,
-            .ss_kf_b             = 2,
-            .ss_kf_c             = 0,
-            .ss_reference_nRoots = TWO,
-            .ss_reference_ans1   = 0,
-            .ss_reference_ans2   = -2,
-        },
-        {
-            .ss_test_n           = 3,
-            .ss_kf_a             = 1,
-            .ss_kf_b             = 0,
-            .ss_kf_c             = 0,
-            .ss_reference_nRoots = ONE,
-            .ss_reference_ans1   = 0,
-            .ss_reference_ans2   = 0,
-        },
-        {
-            .ss_test_n           = 4,
-            .ss_kf_a             = 1,
-            .ss_kf_b             = 2,
-            .ss_kf_c             = 1,
-            .ss_reference_nRoots = ONE,
-            .ss_reference_ans1   = -1,
-            .ss_reference_ans2   = -1,
-        },
-        {
-            .ss_test_n           = 5,
-            .ss_kf_a             = 1,
-            .ss_kf_b             = 0,
-            .ss_kf_c             = 2,
-            .ss_reference_nRoots = NO,
-            .ss_reference_ans1   = NAN,
-            .ss_reference_ans2   = NAN,
-        },
-    };
+    // struct TEST_DATA_solve_square_equation tests[] = {
+    //     {
+    //         .ss_test_n           = 1,
+    //         .ss_kf_a             = 1,
+    //         .ss_kf_b             = -3,
+    //         .ss_kf_c             = 2,
+    //         .ss_reference_nRoots = TWO,
+    //         .ss_reference_ans1   = 1,
+    //         .ss_reference_ans2   = 2,
+    //     },
+    //     {
+    //         .ss_test_n           = 2,
+    //         .ss_kf_a             = 1,
+    //         .ss_kf_b             = 2,
+    //         .ss_kf_c             = 0,
+    //         .ss_reference_nRoots = TWO,
+    //         .ss_reference_ans1   = 0,
+    //         .ss_reference_ans2   = -2,
+    //     },
+    //     {
+    //         .ss_test_n           = 3,
+    //         .ss_kf_a             = 1,
+    //         .ss_kf_b             = 0,
+    //         .ss_kf_c             = 0,
+    //         .ss_reference_nRoots = ONE,
+    //         .ss_reference_ans1   = 0,
+    //         .ss_reference_ans2   = 0,
+    //     },
+    //     {
+    //         .ss_test_n           = 4,
+    //         .ss_kf_a             = 1,
+    //         .ss_kf_b             = 2,
+    //         .ss_kf_c             = 1,
+    //         .ss_reference_nRoots = ONE,
+    //         .ss_reference_ans1   = -1,
+    //         .ss_reference_ans2   = -1,
+    //     },
+    //     {
+    //         .ss_test_n           = 5,
+    //         .ss_kf_a             = 1,
+    //         .ss_kf_b             = 0,
+    //         .ss_kf_c             = 2,
+    //         .ss_reference_nRoots = NO,
+    //         .ss_reference_ans1   = NAN,
+    //         .ss_reference_ans2   = NAN,
+    //     },
+    // };
 
-    size_t n = sizeof(tests) / sizeof(tests[0]);
-    for (size_t i = 0; i<n; ++i) {
+    for (size_t i = 0; i<test_count; ++i) {
         int test_failed = ONE_TEST_solve_square_equation(&tests[i]);
         if (!(test_failed))
             ++*count_tests;
@@ -187,48 +212,74 @@ int TEST_solve_linear_equation(int * const count_tests) {
     (0, 0) -> INF
     */
 
+    FILE *fp = fopen("math_equation_solver/solve_linear_equation_test_data.txt", "r");
+    struct TEST_DATA_solve_linear_equation tests[100];
+    size_t num_of_tests = 0;
+
+    uint16_t num = 0;
+    if (fp) {
+        for(int i = 0;;++i) {
+            double a = 0, b = 0, x = 0;
+            int n_roots = 0;
+            if (fscanf(fp, "#%hd (%lf, %lf) -> %d, %lf", &num, &a, &b, &n_roots, &x)){
+                // printf("#%hd (%lf, %lf) -> %d, %lf\n", num, a, b, n_roots, x);
+
+                tests[i] = {
+                    .ss_test_n = num,
+                    .ss_kf_a = a, .ss_kf_b = b,
+                    .ss_reference_nRoots = (solutions_count) n_roots,
+                    .ss_reference_ans = x,
+                };
+                ++num_of_tests;
+            }
+
+            if (getc(fp) == EOF)
+                break;
+        }
+        fclose(fp);
+    }
+
     int is_failed = 0;
 
-    struct TEST_DATA_solve_linear_equation tests[] = {
-        {
-            .ss_test_n           = 1,
-            .ss_kf_a             = 2,
-            .ss_kf_b             = 0,
-            .ss_reference_nRoots = ONE,
-            .ss_reference_ans    = 0,
-        },
-        {
-            .ss_test_n           = 2,
-            .ss_kf_a             = 2,
-            .ss_kf_b             = 5,
-            .ss_reference_nRoots = ONE,
-            .ss_reference_ans    = -2.5,
-        },
-        {
-            .ss_test_n           = 3,
-            .ss_kf_a             = 0,
-            .ss_kf_b             = 2,
-            .ss_reference_nRoots = NO,
-            .ss_reference_ans    = NAN,
-        },
-        {
-            .ss_test_n           = 4,
-            .ss_kf_a             = 0,
-            .ss_kf_b             = -5,
-            .ss_reference_nRoots = NO,
-            .ss_reference_ans    = NAN,
-        },
-        {
-            .ss_test_n           = 5,
-            .ss_kf_a             = 0,
-            .ss_kf_b             = 0,
-            .ss_reference_nRoots = INF,
-            .ss_reference_ans    = NAN,
-        },
-    };
+    // struct TEST_DATA_solve_linear_equation tests[] = {
+    //     {
+    //         .ss_test_n           = 1,
+    //         .ss_kf_a             = 2,
+    //         .ss_kf_b             = 0,
+    //         .ss_reference_nRoots = ONE,
+    //         .ss_reference_ans    = 0,
+    //     },
+    //     {
+    //         .ss_test_n           = 2,
+    //         .ss_kf_a             = 2,
+    //         .ss_kf_b             = 5,
+    //         .ss_reference_nRoots = ONE,
+    //         .ss_reference_ans    = -2.5,
+    //     },
+    //     {
+    //         .ss_test_n           = 3,
+    //         .ss_kf_a             = 0,
+    //         .ss_kf_b             = 2,
+    //         .ss_reference_nRoots = NO,
+    //         .ss_reference_ans    = NAN,
+    //     },
+    //     {
+    //         .ss_test_n           = 4,
+    //         .ss_kf_a             = 0,
+    //         .ss_kf_b             = -5,
+    //         .ss_reference_nRoots = NO,
+    //         .ss_reference_ans    = NAN,
+    //     },
+    //     {
+    //         .ss_test_n           = 5,
+    //         .ss_kf_a             = 0,
+    //         .ss_kf_b             = 0,
+    //         .ss_reference_nRoots = INF,
+    //         .ss_reference_ans    = NAN,
+    //     },
+    // };
 
-    size_t n = sizeof(tests) / sizeof(tests[0]);
-    for (size_t i = 0; i<n; ++i) {
+    for (size_t i = 0; i<num_of_tests; ++i) {
         int test_failed = ONE_TEST_solve_linear_equation(&tests[i]);
         if (!(test_failed))
             ++*count_tests;
